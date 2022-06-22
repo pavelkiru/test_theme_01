10a87b36398a95d6c57a3a8c6ec2d3c5cc5720c2
Pavel <pavelkirushchenko@gmail.com>
2022-06-22 03:02:55 +0300

    next_5

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -22,24 +22,27 @@ function sunset_add_admin_page() {
   // create admin first submenu page
   add_submenu_page(
     'alecaddd-sunset',
-    'First submenu',
-    'First submenu',
+    'Sunset Settings',
+    'Settings',
     'manage_options',
-    'alecaddd-sunset-first-submenu',
-    'sunset_theme_create_first_submenu',
+    'alecaddd-sunset-settings',
+    'sunset_theme_create_settings',
     1
   );
 
   // create admin second submenu page
-  add_submenu_page(
-    'alecaddd-sunset',
-    'Second submenu',
-    'Second submenu',
-    'manage_options',
-    'alecaddd-sunset-second-submenu',
-    'sunset_theme_create_second_submenu',
-    2
-  );
+//  add_submenu_page(
+//    'alecaddd-sunset',
+//    'Second submenu',
+//    'Second submenu',
+//    'manage_options',
+//    'alecaddd-sunset-second-submenu',
+//    'sunset_theme_create_second_submenu',
+//    2
+//  );
+
+  add_action('admin_init', 'sunset_custom_settings');
+
 }
 
 add_action('admin_menu', 'sunset_add_admin_page');
@@ -48,10 +51,29 @@ function sunset_theme_create_page() {
   require_once get_template_directory() . '/inc/templates/sunset-admin.php';
 }
 
-function sunset_theme_create_first_submenu() {
-  require_once get_template_directory() . '/inc/templates/sunset-admin-first-submenu.php';
+function sunset_theme_create_settings() {
+  require_once get_template_directory() . '/inc/templates/sunset-admin-settings.php';
 }
 
-function sunset_theme_create_second_submenu() {
-  require_once get_template_directory() . '/inc/templates/sunset-admin-second-submenu.php';
+//function sunset_theme_create_second_submenu() {
+//  require_once get_template_directory() . '/inc/templates/sunset-admin-second-submenu.php';
+//}
+
+
+function sunset_custom_settings() {
+  register_setting('sunset-settings-group','first_name');
+  add_settings_section('sunset-sidebar-options','Sidebar Options','sunset_sidebar_options',
+    'alecaddd-sunset'
+  );
+  add_settings_field('sidebar-name', 'First Name', 'sunset_sidebar_name','alecaddd-sunset',
+  'sunset-sidebar-options');
+}
+
+function sunset_sidebar_options() {
+  echo 'Cusomize your Sidebar Information';
+}
+
+function sunset_sidebar_name() {
+  $firstName = esc_attr( get_option('first_name') );
+  echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First name">';
 }
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -2,16 +2,24 @@
 
 
 
-echo '<h1>' . get_admin_page_title() . '</h1>';
 
-$second_menu_link = menu_page_url('alecaddd-sunset-second-submenu', false);
-$first_menu_link = menu_page_url('alecaddd-sunset-first-submenu', false);
+
+//$first_menu_link = menu_page_url('alecaddd-sunset-first-submenu', false);
+//$second_menu_link = menu_page_url('alecaddd-sunset-second-submenu', false);
 
 
 ?>
 <div class="sunset_add_admin_page">
- <ul>
-   <li><a href='<?php echo $first_menu_link ?>'>First submenu</a></li>
-   <li><a href='<?php echo $second_menu_link ?>'>Second submenu</a</li>
- </ul>
+  <?php settings_errors();?>
+  <?php echo '<h1>' . get_admin_page_title() . '</h1>';?>
+  <form method="post" action="options.php" >
+    <?php settings_fields('sunset-settings-group');?>
+    <?php do_settings_sections('alecaddd-sunset');?>
+    <?php submit_button();?>
+  </form>
+
+<!-- <ul>-->
+<!--   <li><a href='--><?php //echo $first_menu_link ?><!--'>First submenu</a></li>-->
+<!--   <li><a href='--><?php //echo $second_menu_link ?><!--'>Second submenu</a</li>-->
+<!-- </ul>-->
 </div>
